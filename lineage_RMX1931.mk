6172033381038987d13da171678933c1f0e6f482
AgnaySrivastava <agnaysrivastva@gmail.com>
2022-04-06 09:21:07 +0530

    Removed extra rom.mk file

--- lineage_RMX1931.mk
+++ lineage_RMX1931.mk
@@ -1,6 +1,9 @@
 #Add full device mk
 $(call inherit-product, device/realme/RMX1931/full_RMX1931.mk)
 
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_RMX1931
@@ -15,3 +18,4 @@ TARGET_VENDOR_PRODUCT_NAME := RMX1931
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="RMX1931-user 11 RKQ1.200928.002 1625457120608 release-keys"
 BUILD_FINGERPRINT := realme/RMX1931/RMX1931L1:11/RKQ1.200928.002/1625457120608:user/release-keys
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
